22d41b069f87a583e34e1b620f94a687709af35c
Artur Oliveira Gomes <>
2022-03-24 19:15:18 -0400

    ex4 adicionado

--- Lab2/ex3.js
+++ Lab2/ex3.js
@@ -35,3 +35,10 @@ function retornaPositivos2(vetor){
 }
 const inicial = [13, -3,  3,  0, -1,  6,  1,-42, 23,  0]
 console.log(retornaPositivos(inicial))
+
+function retornaPositivosEPares(vetor){
+  let novoVetor = [];
+  vetor.map(elem => (elem > 0 (elem%2==0)) && novoVetor.push(elem))
+  return novoVetor;
+}
+console.log(retornaPositivosEPares(inicial))
--- Lab2/ex4.js
+++ Lab2/ex4.js
@@ -1,8 +1,10 @@
 /**
- *  Ler as notas finais de uma turma de 10 alunos armazenando-as em um vetor N. (ok)
+ *  Ler as notas finais de uma turma de 10 alunos armazenando-as em 
+ *  um vetor N. (ok)
  *  A seguir calcule a média aritmética das notas dos alunos aprovados 
  * (nota maior ou igual a 6). 
- * Armazene em um vetor P a posição (índice) que cada aluno ocupa no vetor N, 
+ * Armazene em um vetor P a posição (índice) que cada aluno ocupa 
+ * no vetor N, 
  * dos alunos que obtiveram nota maior que a média calculada. 
  * Imprimir a média calculada e logo após o vetor P.
  * 
@@ -13,3 +15,29 @@
  * 8.58 (média)
  * 2 6 9 (vetor P)
  */
+
+function calculaMedia(n){
+  //declara vetor média como vazio inicialmente
+  let vetMedia = []; 
+  //percorre n e filtra somente os elementos 
+  //maiores ou iguais a 6
+  n.map(elem => elem >= 6 && vetMedia.push(elem))
+  //define variavel para média
+  let media = 0;
+  //percorre o vetor de médias e faz seu somatório
+  vetMedia.map(elem => media = media + elem)
+  //calcula a média sobre o tamanho do vetor vetMedia
+  media = media/vetMedia.length
+  let p = [];
+  n.map((elem,index) => {
+    if(media <=elem){
+      p.push(index)
+    }
+  })
+  // console.log(n)
+  console.log(media)
+  console.log(p)
+
+  
+}
+calculaMedia([8.5, 5, 10, 8, 3.5, 7, 9, 4, 2, 9,])
