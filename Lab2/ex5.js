a6c2aa95bd855392dd915eb7aa9c2ac425246284
Artur Oliveira Gomes <>
2022-03-24 19:35:49 -0400

    exercicio 5 concluido

--- Lab2/ex5.js
+++ Lab2/ex5.js
@@ -14,4 +14,16 @@
  * [22, 34, 33, 53, 52, 41, 13, 14, 27, 81] (vetor Y)
  * [Saída]
  * [11, 22, 5, 34, 10, 33, 8, 53, 23, 52, 12, 41, 91, 13, 21, 14, 7, 27, 9, 81] (vetor R)
- */.
+ */
+
+function permuta(x,y){
+  let novoVetor = []
+  for(var i=0;i<x.length;i++){
+    novoVetor.push(x[i])
+    novoVetor.push(y[i])
+  }
+  return novoVetor
+}
+const vetx = [1,3,5,7,9]
+const vety = [0,2,4,6,8]
+console.log(permuta(vety,vetx))
