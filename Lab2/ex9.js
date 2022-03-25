f3012e54a6e22ab3d9faa40a2b6f0a3e545120e3
Artur Oliveira Gomes <>
2022-03-24 20:16:54 -0400

    exercicio 9 pronto

--- Lab2/ex9.js
+++ Lab2/ex9.js
@@ -1,7 +1,16 @@
 /**
- * Faça um programa que recebe a altura (em metros) 
- * e o sexo de uma pessoa e retorna o seu peso ideal 
+ * Faça um programa que recebe a altura 
+ * (em metros) 
+ * e o sexo de uma pessoa e retorna o seu 
+ * peso ideal 
  * através da seguinte fórmula: 
  * • Homens: peso = 72.7 * altura - 58; 
  * • Mulheres: peso = 62.1 * altura - 44.7;
  */
+
+const homensPeso = 
+  altura => 72.7 * altura - 58; 
+const mulheresPeso = 
+  altura => 62.1 * altura - 44.7;
+
+console.log(mulheresPeso(1.65))
