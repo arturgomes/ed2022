410561e4a49bf2bbed32a8fc01ad73952befef0a
Artur Oliveira Gomes <>
2022-03-24 20:05:23 -0400

    exercicio 7 concluído

--- Lab2/ex7.js
+++ Lab2/ex7.js
@@ -18,3 +18,22 @@
  * Vetor B | 80 | 13 | -3 |  3 |  0 | -1 |  6 |  1 |-42 | 23 | 
  *         +----+----+----+----+----+----+----+----+----+----+
 */
+
+function insereNoInicio(vetor,x){
+  let aux = []
+  aux[0] = x;
+  for(let i = 0; i< vetor.length; i++){
+    aux[i+1] = vetor[i];
+  }
+  return aux;
+}
+
+console.log(insereNoInicio([13, -3,  3,  0, -1, 6 ,  1, -42, 23], 80))
+
+function insereNoInicio2(vetor,x){
+  let aux = [];
+  aux.push(x)
+  vetor.map(elem => aux.push(elem))
+  return aux;
+}
+console.log(insereNoInicio2([13, -3,  3,  0, -1, 6 ,  1, -42, 23], 80))
